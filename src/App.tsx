729912c8f89f89531685ceec3a9bf65d38c61d3a
Kirill Kubryakov <kubryakov@gmail.com>
2023-12-14 22:58:31 +0200

    Show current position in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { FC } from "react";
 import { getPoints } from "./api";
 import { useData } from "./useData";
 import { PointsList } from "./PointsList";
+import { useCurrentPosition } from "./useCurrentPosition";
 
 const App: FC = () => {
   const [points, status] = useData(getPoints);
+  const [position, positionStatus] = useCurrentPosition();
 
   return (
     <>
       <h1>Star Wars Locator</h1>
+      {positionStatus === "success" && (
+        <div>location: {JSON.stringify(position)}</div>
+      )}
       {status === "success" && <PointsList points={points} />}
     </>
   );
